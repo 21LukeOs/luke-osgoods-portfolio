1cd90512682f6024feb7332f2af8a1ec5a024037
Luke Osgood <lukemosgood@gmail.com>
2020-08-24 02:14:08 -0400

    title animation, rough phone component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.scss';
 import Nav from './components/Nav/Nav';
 
 function App() {
-  return (
-    <div className="app">
-      <Nav />
-      <header className="app-header">
-        <h1>Luke Osgood</h1>
-      </header>
-    </div>
-  );
+	return (
+		<div className='app'>
+			<Nav />
+			<div className='app-heading'>
+				<h1 className='fade-slide-in'>Luke Osgood</h1>
+			</div>
+      <div className='phones'>
+        
+      </div>
+		</div>
+	);
 }
 
 export default App;
