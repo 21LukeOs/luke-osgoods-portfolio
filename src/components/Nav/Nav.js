0dfea71b381dcb313b48533f144c34c202f92db3
Luke Osgood <lukemosgood@gmail.com>
2020-08-26 09:40:29 -0400

    added css vendor prefixes, fixed issue with nav in safari

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,7 +5,7 @@ import logo from '../../assets/logo-512.png';
 function Nav() {
 	return (
 		<div className='nav'>
-			<img src={logo} alt='logo' className='nav__logo' width='50px' />
+			<img src={logo} alt='logo' className='nav__logo' width='50px' height='50px' />
 			<a href='https://lukeosgood.com' target='_blank' rel='noopener noreferrer'><button className='nav__resume-button'>Resumé</button></a>
 		</div>
 	);
